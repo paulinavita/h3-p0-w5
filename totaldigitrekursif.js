a873ad4a760d9fe8ed1faacd039a5b938ba4c841
paulinavita <paulinadavita@gmail.com>
2019-01-23 11:56:59 +0700

    revise structure

--- totaldigitrekursif.js
+++ totaldigitrekursif.js
@@ -6,36 +6,34 @@ function totalDigitRekursif(angka) {
     if (stringNum.length === 1) {
         return Number(stringNum)
     }
-
     //PARAMETER BARU
     var lastIndex = stringNum.length - 1
-
     return Number(stringNum[0]) + totalDigitRekursif(Number(stringNum.slice(1,stringNum.length)))
 
 }
 
-function totalDigitTobi(angka) {
-    let strAngka = String(angka);
-    
-    //STOPPER
-    if ( strAngka.length == 1 ) {
-        return angka;
-    };
-
-    //PARAMETER BERIKUTNYA MAU APA
-    let angkaLebih = String(angka).slice(2, angka.length);
-
-    let angkaBaru = Number(strAngka[0]) + Number(strAngka[1]);
-
-    return angkaBaru + totalDigitTobi(Number(angkaLebih))
-
-}
-
-
   
   // TEST CASES
-  console.log(totalDigitTobi(1542)); // 8
+  console.log(totalDigitRekursif(1542)); // 8
 //   console.log(totalDigitRekursif(1542)); // 12
 //   console.log(totalDigitRekursif(5)); // 5
 //   console.log(totalDigitRekursif(21)); // 3
   console.log(totalDigitRekursif(11111)); // 5
+
+
+// function totalDigitCoba(angka) {
+//     let strAngka = String(angka);
+    
+//     //STOPPER
+//     if ( strAngka.length == 1 ) {
+//         return angka;
+//     };
+
+//     //PARAMETER BERIKUTNYA MAU APA
+//     let angkaLebih = String(angka).slice(2, angka.length);
+
+//     let angkaBaru = Number(strAngka[0]) + Number(strAngka[1]);
+
+//     return angkaBaru + totalDigitTobi(Number(angkaLebih))
+
+// }
